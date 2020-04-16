dadfa46249bb6ff0b1f5a26af8d52e28dcfcedea
Ewerton Marques <marquescpv@gmail.com>
2020-04-16 06:48:31 -0300

    Incluindo as rotas

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Fragment } from 'react';
 import { Notification, Section } from 'rbx';
 import "./App.scss";
 import HomeScreen from './screens/home';
+import Routes from './routes';
 
 const App = () => (
   <Fragment>
-    <HomeScreen/>
+    <Routes />
   </Fragment>
 )
 
