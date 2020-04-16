add3495134f5dd16096d75d205f17b6012044270
Ewerton Marques <marquescpv@gmail.com>
2020-04-16 17:09:18 -0300

    Incluindo a busca

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,45 +1,38 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from 'react';
 import { Column, Button } from "rbx";
 import "../../styles/notes.scss";
-import { push as Menu } from "react-burger-menu";
+import { push as Menu } from 'react-burger-menu'
 import List from "../notes/list";
 import Editor from "../notes/editor";
-import NoteService from "../../services/notes";
+import Search from "../notes/search";
+import NotesService from '../../services/notes';
 
 function Notes(props) {
   const [notes, setNotes] = useState([]);
-  const [current_note, setCurrentNote] = useState({
-    title: "",
-    body: "",
-    id: "",
-  });
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
 
   async function fetchNotes() {
-    const response = await NoteService.index();
+    const response = await NotesService.index();
     if (response.data.length >= 1) {
-      setNotes(response.data.reverse());
-      setCurrentNote(response.data[0]);
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
     } else {
       setNotes([]);
     }
   }
 
-  const createNote = async (params) => {
-    const note = await NoteService.create();
+  const createNote = async () => {
+    await NotesService.create();
     fetchNotes();
-  };
+  }
 
   const deleteNote = async (note) => {
-    await NoteService.delete(note._id);
+    await NotesService.delete(note._id);
     fetchNotes();
-  };
+  }
 
   const updateNote = async (oldNote, params) => {
-    const updatedNote = await NoteService.update(oldNote._id, params);
+    const updatedNote = await NotesService.update(oldNote._id, params);
     const index = notes.indexOf(oldNote);
     const newNotes = notes;
     newNotes[index] = updatedNote.data;
@@ -47,16 +40,25 @@ function Notes(props) {
     setCurrentNote(updatedNote.data);
   }
 
+  const searchNotes = async (query) => {
+    const response = await NotesService.search(query);
+    setNotes(response.data);
+  }
+
   const selectNote = (id) => {
     const note = notes.find((note) => {
       return note._id == id;
-    });
+    })
     setCurrentNote(note);
-  };
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
   return (
     <Fragment>
-      <div className="notes" id="notes">
+      <Column.Group className="notes" id="notes">
         <Menu
           pageWrapId={"notes-editor"}
           isOpen={props.isOpen}
@@ -66,24 +68,29 @@ function Notes(props) {
           customBurgerIcon={false}
           customCrossIcon={false}
         >
+          <Column.Group>
+            <Column size={10} offset={1}>
+              <Search searchNotes={searchNotes} fetchNotes={fetchNotes}/>
+            </Column>
+          </Column.Group>
           <List
             notes={notes}
             selectNote={selectNote}
+            current_note={current_note} 
             deleteNote={deleteNote}
-            current_note={current_note}
-            createNote={createNote}
-          />
+            createNote={createNote}/>
         </Menu>
+
 
         <Column size={12} className="notes-editor" id="notes-editor">
           <Editor 
             note={current_note}
-            updateNote={updateNote} 
+            updateNote={updateNote}  
           />
         </Column>
-      </div>
+      </Column.Group>
     </Fragment>
-  );
+  )
 }
 
 export default Notes;
